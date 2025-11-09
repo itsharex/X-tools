f0a344ba618b356e31bc567ba31a490ba75f235c
yan <yqp_001@163.com>
2025-11-09 20:44:32 +0800

    实现基本布局，标题栏等。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {ConfigProvider, Splitter} from "antd";
 
 export const App: React.FC = () => {
-  return (
-    <div className="app">
-      <h1>X-tools</h1>
-    </div>
-  );
+    return (
+        <ConfigProvider
+            theme={{
+                components: {
+                    Splitter: {
+                        splitBarDraggableSize: 0,
+                    },
+                },
+            }}
+        >
+            <Splitter style={{height: '100vh'}}>
+                <Splitter.Panel defaultSize={320} min={80}>
+                    <div className={'top-bar'} style={{paddingLeft: 80}}>X-tools</div>
+                </Splitter.Panel>
+                <Splitter.Panel min={240}>
+                    <div className={'top-bar'}>text</div>
+                </Splitter.Panel>
+                <Splitter.Panel defaultSize={320} min={80}>
+                    <div className={'top-bar'}>text</div>
+                </Splitter.Panel>
+            </Splitter>
+        </ConfigProvider>
+    );
 };
